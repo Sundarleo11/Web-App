20e61f63ad953e26f3e210e29bdd6abf8be2dff2
Sundarleo11 <sundarrock1999@gmail.comm>
2022-01-17 13:06:33 +0530

    requestHandler
    Signed-off-by: Sundarleo11 <sundarrock1999@gmail.comm>

--- basichttpserver/server.js
+++ basichttpserver/server.js
@@ -1,7 +1,12 @@
 const http=require('http');
 const port =8085;
 
-const server=http.createServer();
+function requestHandler(req,res){
+    console.log(req.url);
+    res.end("Have a nice day !");
+}
+
+const server=http.createServer(requestHandler);
 
 server.listen(port,(err)=>{
     if(err) throw err;
