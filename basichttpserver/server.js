2b271fbf1781f090f6dbe20bea8f8256e77e1de6
Sundarleo11 <sundarrock1999@gmail.comm>
2022-01-17 19:51:30 +0530

    multiplepage
    Signed-off-by: Sundarleo11 <sundarrock1999@gmail.comm>

--- basichttpserver/server.js
+++ basichttpserver/server.js
@@ -2,20 +2,40 @@ const http=require('http');
 const port =8085;
 const fs=require('fs');
 
+
 function requestHandler(req,res){
-    console.log(req.url);
+  //  console.log(req.url);
    // res.writeHead(200,{'content-type':'text/html'});
-   fs.readFile('./index.html',function(err,data){
+   /*fs.readFile('./index.html',function(err,data){
        if(err){
            console.log('err',err);
            return res.end("<h1>Error</h1>");
        }
        return res.end(data);
    })
-   
-   
-   
-   // res.end("<h4>Have a nice day !</h4>");
+    // res.end("<h4>Have a nice day !</h4>");*/
+
+    let filepath;
+    switch(req.url){
+        case '/':
+            filepath='./index.html';
+            break;
+        case '/profile':
+            filepath='./profile.html';
+            break;
+         default:
+            filepath='./404.html';
+            break;
+
+    }
+    fs.readFile(filepath,function(err,data){
+        if(err){
+            console.log('err',err);
+            return res.end("<h1>Error</h1>");
+        }
+        return res.end(data);
+    })
+
 }
 
 const server=http.createServer(requestHandler);
