272ebf21074b1e0c6ee59fcbcaad7a7113183f72
Sundarleo11 <sundarrock1999@gmail.comm>
2022-01-18 15:10:00 +0530

    urlencode
    Signed-off-by: Sundarleo11 <sundarrock1999@gmail.comm>

--- Express/Startframeworks/server.js
+++ Express/Startframeworks/server.js
@@ -6,12 +6,17 @@ const app=express();
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded());
+
+
 var contactList=[
      {name:'sundar',phone:8870829330},
      {name:'Manisha',phone:283932330},
      {name:'Kumar',phone:1239438403},
      {name:'kamala',phone:29383920},
 ]
+
+
 app.get('/',(req,res)=>{
     // console.log(req);
     // res.send('<h2>Cool, it is running !</h2>');
@@ -22,11 +27,30 @@ app.get('/',(req,res)=>{
     });
 })
 
+
 app.get('/index',(req,res)=>{
     return res.render('index',{
         title:'index'
     })
 })
+
+
+app.post('/create-contact',(req,res)=>{
+  /// return res.redirect('/home');
+    //  console.log(req);
+    //   console.log(req.body);
+    //   console.log(req.body.name);
+    //  console.log(req.body.phone);
+    // contactList.push({
+    //     name:req.body.name,
+    //     phone:req.body.phone
+    // });
+
+    contactList.push(req.body);
+
+    return res.redirect('/');
+})
+
 
 app.listen(port,(err,data)=>{
     if(err){
