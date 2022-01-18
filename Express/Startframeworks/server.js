9326d652230949626fcf03ed4e6ee45816846b08
Sundarleo11 <sundarrock1999@gmail.comm>
2022-01-18 13:31:32 +0530

    rendered ejs page
    Signed-off-by: Sundarleo11 <sundarrock1999@gmail.comm>

--- Express/Startframeworks/server.js
+++ Express/Startframeworks/server.js
@@ -5,13 +5,21 @@ const port=8081;
 const app=express();
 
 app.set('view engine','ejs');
-// app.set('views',path.join(__dirname,'views'));
+app.set('views',path.join(__dirname,'views'));
 
 app.get('/',(req,res)=>{
     // console.log(req);
     // res.send('<h2>Cool, it is running !</h2>');
 
-    return res.render('home');
+    return res.render('home',{
+        title:'First ejs page );'
+    });
+})
+
+app.get('/index',(req,res)=>{
+    return res.render('index',{
+        title:'index'
+    })
 })
 
 app.listen(port,(err,data)=>{
