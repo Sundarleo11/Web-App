848e64ceb1ddd4edf57b2ef5feba4ff70671654b
Sundarleo11 <sundarrock1999@gmail.comm>
2022-01-19 15:21:32 +0530

    model
    Signed-off-by: Sundarleo11 <sundarrock1999@gmail.comm>

--- Express/Startframeworks/server.js
+++ Express/Startframeworks/server.js
@@ -2,6 +2,7 @@ const express=require('express');
 const path=require('path');
 const port=8081;
 const db=require('./config/mongoose');
+const Contact=require('./model/Contact');
 
 const app=express();
 
