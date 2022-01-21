31ae769bc890f047b6d5fb051d3c5138fbf412d7
Sundarleo11 <sundarrock1999@gmail.comm>
2022-01-21 08:17:50 +0530

    Rendering Pages for Sign up and Sign in
    Signed-off-by: Sundarleo11 <sundarrock1999@gmail.comm>

--- ExpressApp/Controller/user_controller.js
+++ ExpressApp/Controller/user_controller.js
@@ -1,3 +1,17 @@
+
 module.exports.profile=function(req,res){
     return res.end('<h1>user profile</h1>');
+}
+
+
+module.exports.signin=function(req,res){
+    return res.render('user_sign_in',{
+        title:'sign-in'
+    })
+}
+
+module.exports.signup=function(req,res){
+    return res.render('user_sign_up',{
+        title:'sign-up'
+    })
 }
--- ExpressApp/router/user.js
+++ ExpressApp/router/user.js
@@ -1,7 +1,8 @@
 const router=require('express').Router();
 const Controller=require('../Controller/user_controller');
 
-
+router.get('/sign_in',Controller.signin);
+router.get('/sign_up',Controller.signup);
 
 router.get('/profile',Controller.profile);
 
