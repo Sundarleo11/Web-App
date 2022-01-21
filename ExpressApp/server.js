6170cf1027969912398c98cc7c9cef49ab3ee356
Sundarleo11 <sundarrock1999@gmail.comm>
2022-01-21 08:49:35 +0530

    Express Ejs layout
    Signed-off-by: Sundarleo11 <sundarrock1999@gmail.comm>

--- ExpressApp/server.js
+++ ExpressApp/server.js
@@ -2,11 +2,30 @@ const express=require('express');
 const port=8000;
 const app=express();
 
+
+const expressLayouts = require('express-ejs-layouts');
+
+
+
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
+
+
 //Added router for customer any request
 app.use('/',require('./router'));
+
 //setup the views engine
 app.set('view engine','ejs');
 app.set('views', './views');
+
+
 app.listen(port,function(err){
     if(err){
         console.log(`error on server port:${port}`);
