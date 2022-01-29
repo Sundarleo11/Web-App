64cc306994a4e6b86d0aabf3683a8f46459d6201
Sundarleo11 <sundarrock1999@gmail.comm>
2022-01-29 10:55:37 +0530

    Setting up Mongo store for session cookies
    Signed-off-by: Sundarleo11 <sundarrock1999@gmail.comm>

--- ExpressApp/server.js
+++ ExpressApp/server.js
@@ -2,23 +2,20 @@ const express=require('express');
 const port=8000;
 const app=express();
 const cookies=require('cookie-parser');
-
 const DB=require('./Config/mongoose');
 const expressLayouts = require('express-ejs-layouts');
-
 //session cookiess
 const passport=require('passport');
 const passport_local=require('./Config/passport_local_startegy');
 const session=require('express-session');
-
+//const MongoStore=require('connect-mongo');
+const MongoStore = require('connect-mongo')(session);
 
 
 
 app.use(express.urlencoded());
 app.use(cookies());
-
 app.use(express.static('./assets'));
-
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
@@ -30,7 +27,7 @@ app.set('layout extractScripts', true);
 app.set('view engine','ejs');
 app.set('views', './views');
 
-
+//mongo stroe use to store the session cookie in db
 app.use(session({
     name: 'ExpressApp',
     // TODO change the secret before deployment in production mode
@@ -39,13 +36,24 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
+    }, 
+    store: new MongoStore(
+        {
+            mongooseConnection: DB,
+            autoRemove: 'disabled'
+        
+        },
+        function(err){
+            console.log(err ||  'connect-mongodb setup ok');
+        }
+    )
 }));
+
+
+
 
 app.use(passport.initialize());
 app.use(passport.session());
-
-
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
@@ -54,6 +62,8 @@ app.use(passport.setAuthenticatedUser);
 app.use('/',require('./router'));
 
 
+
+//Main server
 app.listen(port,function(err){
     if(err){
         console.log(`error on server port:${port}`);
