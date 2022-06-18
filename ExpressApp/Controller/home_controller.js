68a695987cbf66bc07c75b8dc1f34456a43ace18
sundarleo11 <sundarrock1999@gmail.com>
2022-06-18 10:54:52 +0530

    display the user name

--- ExpressApp/Controller/home_controller.js
+++ ExpressApp/Controller/home_controller.js
@@ -7,13 +7,22 @@ module.exports.home=function(req,res){
    /*  return res.render('home',{
          title:'Home'
      })*/
+/*
 Post.find({},function(err,posts){
 
     return res.render('home',{
         title:'Home',
         posts:posts
     })
+})*/
+
+// populate the user of each post
+Post.find({ }).populate("user").exec(function(err, posts){
+
+    return res.render('home',{
+        title:'Home',
+        posts:posts
+    });
 })
 
-     
 }
