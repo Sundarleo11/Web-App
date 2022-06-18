fe9b87ebe8a8b2dc2a274e8a7445be74e60c2766
sundarleo11 <sundarrock1999@gmail.com>
2022-06-18 19:27:26 +0530

    createdAuthentication

--- ExpressApp/router/post.js
+++ ExpressApp/router/post.js
@@ -1,8 +1,9 @@
 const express=require('express');
 const router=express.Router();
+const passport=require('passport');
 
 const postControler=require('../Controller/post_controller');
 
-router.post('/create',postControler.create);
+router.post('/create',passport.checkAuthentication,postControler.create);
 
 module.exports=router;
